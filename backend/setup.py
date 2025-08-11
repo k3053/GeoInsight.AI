400bf6dd97d8809595975d13692969f795143fc0
Meet Jariwala <meetjari0512@gmail.com>
2025-08-12 00:11:37 +0530

    added structure for backend

---
+++ backend/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages 
+
+setup(
+    name="GeoInsight.AI",
+    author="Meet Jariwala",
+    version="0.1",
+    packages=find_packages(),
+    python_requires=">=3.8",
+    install_requires=[
+        "structlog",
+    ],
+)
